0f0469866295a6202ebe93224cae45d10bf1ba6f
ToniTani <toni.tani@student.saimia.fi>
2020-12-27 23:05:59 +0200

    machinecard-start component as a starting component

---
+++ src/app/machinecards/machinecard-start/machinecard-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machinecard-start',
+  templateUrl: './machinecard-start.component.html',
+  styleUrls: ['./machinecard-start.component.css']
+})
+export class MachinecardStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
