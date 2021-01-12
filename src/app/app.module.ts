a04e31d0225dc88cc88c9285e246f6afbc1cba3a
ToniTani <toni.tani@student.saimia.fi>
2021-01-12 13:59:05 +0200

    jonne changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,34 +11,21 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
 import { MachinecardListviewComponent } from './machinecard-listview/machinecard-listview.component';
 import {MatExpansionModule} from '@angular/material/expansion';
-import { MachinecardsComponent } from './machinecards/machinecards.component';
-import { CardListComponent } from './machinecards/card-list/card-list.component';
-import { CardDetailComponent } from './machinecards/card-detail/card-detail.component';
-import { CardItemComponent } from './machinecards/card-list/card-item/card-item.component';
 import { MachinecardNotInUseListviewComponent } from './machinecard-not-in-use-listview/machinecard-not-in-use-listview/machinecard-not-in-use-listview.component';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatCardModule} from '@angular/material/card';
-import { MachinecardStartComponent } from './machinecards/machinecard-start/machinecard-start.component';
-import { CardEditComponent } from './machinecards/card-edit/card-edit.component';
+import { MachinecardDetailComponent } from './machinecard-detail/machinecard-detail/machinecard-detail.component';
+import {FormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {ReactiveFormsModule} from '@angular/forms';
-import {CardService} from './machinecards/card.service';
-import {CardResolverService} from './machinecards/card-resolver.service';
+import {MatCardModule} from '@angular/material/card';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatInputModule} from '@angular/material/input';
-
-
 
 const appRoutes: Routes = [
 
-  {path: '', redirectTo: '/kortit', pathMatch: 'full' },
-  {path: 'kortit', component: MachinecardListviewComponent},
-  {path: 'deviceActiveFalse', component: MachinecardNotInUseListviewComponent},
-  {path: 'machinecards', component: MachinecardsComponent, children: [
-      {path: '', component: MachinecardStartComponent},
-      {path: 'new', component: CardEditComponent, resolve: [CardResolverService]},
-      {path: ':id', component: CardDetailComponent, resolve: [CardResolverService]},
-      {path: ':id/edit', component: CardEditComponent}
-    ]},
+  {path: '', redirectTo: '/Machinecards', pathMatch: 'full' },
+  {path: 'Machinecards', component: MachinecardListviewComponent},
+  {path: 'Machinecards/new', component: MachinecardDetailComponent},
+  {path: 'Machinecards/:id', component: MachinecardDetailComponent},
+  {path: 'DeviceActiveFalse', component: MachinecardNotInUseListviewComponent}
 ];
 
 @NgModule({
@@ -47,13 +34,7 @@ const appRoutes: Routes = [
     HeaderComponent,
     MachinecardListviewComponent,
     MachinecardNotInUseListviewComponent,
-    MachinecardsComponent,
-    CardListComponent,
-    CardDetailComponent,
-    CardItemComponent,
-    MachinecardNotInUseListviewComponent,
-    MachinecardStartComponent,
-    CardEditComponent
+    MachinecardDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -65,13 +46,13 @@ const appRoutes: Routes = [
     MatExpansionModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    MatMenuModule,
-    MatCardModule,
+    FormsModule,
     MatFormFieldModule,
-    ReactiveFormsModule,
     MatInputModule,
+    MatCardModule,
+    MatSnackBarModule
   ],
-  providers: [CardService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
