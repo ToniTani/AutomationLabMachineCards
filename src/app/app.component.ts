12e5c3d7c2dfda9e11aa4091503485c6da2126e0
ToniTani <toni.tani@student.saimia.fi>
2021-01-21 01:36:24 +0200

    custom icons are now working from assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'automation-lab-cards';
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'playing-cards',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/playing-cards.svg')
+    )
+  }
 }
